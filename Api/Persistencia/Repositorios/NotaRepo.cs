0382fe88bf0b935fb01ecabae298ef3652bfd9d8
mmanna3 <mmmannna3@gmail.com>
2025-08-24 12:15:42 -0300

    funciona edición de escritor

--- Api/Persistencia/Repositorios/NotaRepo.cs
+++ Api/Persistencia/Repositorios/NotaRepo.cs
@@ -9,4 +9,10 @@ public class NotaRepo : RepositorioABM<Nota>, INotaRepo
     public NotaRepo(AppDbContext context) : base(context)
     {
     }
+    
+    protected override void AntesDeModificar(Nota entidadAnterior, Nota entidadNueva)
+    {
+        entidadNueva.CarpetaId = entidadAnterior.CarpetaId;
+        entidadNueva.FechaHora = DateTime.Now;
+    }
 }
