68039c84c5a7d78dbcd7511b350c2e01e78d6327
mmanna3 <mmmannna3@gmail.com>
2025-08-26 00:08:55 -0300

    fechas creación y edición en NotaDTO

--- Api/Core/DTOs/NotaDTO.cs
+++ Api/Core/DTOs/NotaDTO.cs
@@ -7,7 +7,8 @@ public class NotaDTO : DTO
     [Required]
     public required string Titulo { get; set; }
     public string? Cuerpo { get; set; }
-    public DateTime FechaHora { get; set; }
+    public DateTime? FechaHoraCreacion { get; set; }
+    public DateTime? FechaHoraEdicion { get; set; }
     public int CarpetaId { get; set; }
     public string? CarpetaTitulo { get; set; }
 }
