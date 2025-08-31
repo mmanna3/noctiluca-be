d850463ac301896a095857a5153463677e4c50b6
mmanna3 <mmmannna3@gmail.com>
2025-08-30 22:32:08 -0300

    El título de la carpeta bien mapeado en los escritos

--- Api/Persistencia/Repositorios/EscritoRepo.cs
+++ Api/Persistencia/Repositorios/EscritoRepo.cs
@@ -1,6 +1,7 @@
 using Api.Core.Entidades;
 using Api.Core.Repositorios;
 using Api.Persistencia._Config;
+using Microsoft.EntityFrameworkCore;
 
 namespace Api.Persistencia.Repositorios;
 
@@ -8,6 +9,11 @@ public class EscritoRepo : RepositorioABM<Escrito>, IEscritoRepo
 {
     public EscritoRepo(AppDbContext context) : base(context)
     {
+    }
+    
+    protected override IQueryable<Escrito> Set()
+    {
+        return Context.Set<Escrito>().Include(x => x.Carpeta);
     }
     
     protected override void AntesDeModificar(Escrito entidadAnterior, Escrito entidadNueva)
