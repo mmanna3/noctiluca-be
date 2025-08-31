d3ca2b3e3b10d9ea944099a04481d8b7776181a4
mmanna3 <mmmannna3@gmail.com>
2025-08-31 00:01:18 -0300

    se pueden eliminar carpetas sin escritos

--- Api/Core/Servicios/CarpetaCore.cs
+++ Api/Core/Servicios/CarpetaCore.cs
@@ -1,5 +1,6 @@
 using Api.Core.DTOs;
 using Api.Core.Entidades;
+using Api.Core.Otros;
 using Api.Core.Repositorios;
 using Api.Core.Servicios.Interfaces;
 using AutoMapper;
@@ -10,5 +11,18 @@ public class CarpetaCore : ABMCore<ICarpetaRepo, Carpeta, CarpetaDTO>, ICarpetaC
 {
     public CarpetaCore(IBDVirtual bd, ICarpetaRepo repo, IMapper mapper) : base(bd, repo, mapper)
     {
+    }
+    
+    public override async Task Eliminar(int id)
+    {
+        var carpeta = await Repo.ObtenerPorId(id);
+        if (carpeta == null)
+            throw new ExcepcionControlada("No existe la carpeta a eliminar");
+        
+        if (carpeta.Escritos.Count != 0)
+            throw new ExcepcionControlada("Para eliminar la carpeta, eliminá los escritos primero");
+        
+        Repo.Eliminar(carpeta);
+        await BDVirtual.GuardarCambios();
     }
 }
